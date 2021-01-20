f16623b86bae29173f50df9e6539bb71784d7cdd
Weixin Qi <s4590288@gmail.com>
2021-01-20 18:23:30 +1000

    fix: 🐛 the svg can not be bundle in webpack
    --add the svg-url-loader to solve svg problem

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,18 @@ module.exports = {
             loader: 'file-loader',
           },
         ],
-      }
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
     ]
   },
   plugins: [
