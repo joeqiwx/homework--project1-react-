1e2744227a705bda9227ab60e66573bc07a4f815
Weixin Qi <s4590288@gmail.com>
2021-01-21 11:16:56 +1000

    feat: 🎸 Add new pages component
    add three new pages componet in pages component

--- src/components/APP/components/Pages/Pages.js
+++ src/components/APP/components/Pages/Pages.js
@@ -2,10 +2,17 @@ import Home from './components/Home/Home'
 import './Pages.css'
 import './components/Home/Home'
 import Resume from './components/Resume/Resume'
+import Services from './components/Services/Services'
+import Blog from './components/Blog/Blog'
+import Contact from './components/Contact/Contact'
+
 const Pages = () => (
     <main>
         <Home/>
         <Resume/>
+        <Services/>
+        <Blog />
+        <Contact />
     </main>
 )
 
