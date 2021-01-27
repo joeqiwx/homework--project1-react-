c977223f55d5801b349731e4859509dfbcd0bd20
Weixin Qi <s4590288@gmail.com>
2021-01-27 10:01:20 +1000

    fix: 🐛 change the function componet app to class component
    make app to be a class component, make variable currentPage save in
    state

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './src/components/APP/App';
 import './assets/css/style.css';
-const render = () => (ReactDOM.render(
-    <App render={render} />,
-    document.getElementById('root')));
-render();
+ReactDOM.render(
+    <App />,
+    document.getElementById('root'));
--- src/components/APP/App.js
+++ src/components/APP/App.js
@@ -2,22 +2,44 @@ import Header from "./components/Header/Header"
 import Pages from "./components/Pages/Pages"
 import Footer from "./components/Footer/Footer";
 import '../../../assets/css/style.css'
-const obj = {
-    currentPage : "Home"
-}
-const App = ({render}) => {
+// const obj = {
+//     currentPage : "Home"
+// }
+// const App = ({render}) => {
 
-    const changePage = (pageName) => {
-        obj.currentPage = pageName;
-        render();
+//     const changePage = (pageName) => {
+//         obj.currentPage = pageName;
+//         render();
+//     }
+//     return(
+//     <div>
+//         <Header currentPage={obj.currentPage} 
+//                 changePage={changePage}/>
+//         <Pages currentPage={obj.currentPage}/>
+//         <Footer/>
+//     </div>
+// )};
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentPage : 'Home',
+        }
     }
-    return(
-    <div>
-        <Header currentPage={obj.currentPage} 
-                changePage={changePage}/>
-        <Pages currentPage={obj.currentPage}/>
-        <Footer/>
-    </div>
-)};
-
+    render() {
+        const {currentPage} = this.state;
+        const changePage = (pageName) => {
+            this.setState({currentPage : pageName});
+        }
+        return (
+            <div>
+                <Header currentPage={currentPage} 
+                        changePage={changePage}/>
+                <Pages currentPage={currentPage}/>
+                <Footer/>
+            </div>
+        )
+    }
+}
 export default App;
